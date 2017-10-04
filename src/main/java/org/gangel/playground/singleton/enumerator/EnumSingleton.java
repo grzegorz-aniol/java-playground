48be637f7347ba23c42373576546726e863f483d
Grzegorz Aniol <Grzegorz_Aniol@epam.com>
2017-10-04 09:50:59 +0200

    Singleton pattern with enum example

---
+++ src/main/java/org/gangel/playground/singleton/enumerator/EnumSingleton.java
@@ -0,0 +1,13 @@
+package org.gangel.playground.singleton.enumerator;
+
+public enum EnumSingleton {
+
+    INSTANCE;
+    
+    public int getInt() {
+        return 0;
+    }
+    
+    public void setSomething(int val) {        
+    }
+}
---
+++ src/main/java/org/gangel/playground/singleton/enumerator/ShowEnumSingletone.java
@@ -0,0 +1,11 @@
+package org.gangel.playground.singleton.enumerator;
+
+public class ShowEnumSingletone {
+
+    public static void main(String[] args) {
+        
+        System.out.println(EnumSingleton.INSTANCE.hashCode());
+        
+    }
+    
+}
